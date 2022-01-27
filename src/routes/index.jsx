189445483de36e8f0fefd6d705041bc1579ee837
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-01-27 19:23:03 -0300

    feat - organizado pastas e construido rotas

---
+++ src/routes/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/foods" component={ MainFoods } />
+      <Route exact path="/drinks" component={ MainDrinks } />
+      <Route exact path={ `foods/${recipeId}` } component={ FoodDetails } />
+      <Route exact path={ `drinks/${recipeId}` } component={ DrinkDetails } />
+      <Route
+        exact
+        path={ `foods/${recipeId}/in-progress` }
+        component={ FoodInProgress }
+      />
+      <Route
+        exact
+        path={ `drinks/${recipeId}/in-progress` }
+        component={ DrinkInProgress }
+      />
+      <Route exact path="/explore" component={ Explore } />
+      <Route exact path="/explore/foods" component={ ExploreFoods } />
+      <Route exact path="/explore/drinks" component={ ExploreDrinks } />
+      <Route
+        exact
+        path="/explore/foods/ingredients"
+        component={ ExploreFoodsIngredients }
+      />
+      <Route
+        exact
+        path="/explore/drinks/ingredients"
+        component={ ExploreDrinksIngredients }
+      />
+      <Route
+        exact
+        path="/explore/foods/nationalities"
+        component={ ExploreFoodsNationalities }
+      />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+    </Switch>
+  );
+}
+
+export default Routes;
