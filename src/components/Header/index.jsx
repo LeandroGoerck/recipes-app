ea159e8b33cd07fa7264016030c20d126080cac9
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-01-28 17:22:28 -0300

    feat: req/12 - Adiciona lógica de exibir ou não campo de busca através do icon search

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
 function Header({ children, displayIconSearch }) {
+  const [displayInputSearch, setDisplayInputSearch] = useState(false);
+
   return (
     <header>
       <Link to="/profile">
@@ -12,7 +14,18 @@ function Header({ children, displayIconSearch }) {
       </Link>
       <p data-testid="page-title">{children}</p>
       {displayIconSearch !== false && (
-        <img data-testid="search-top-btn" src={ searchIcon } alt="Search icon" />)}
+        <button
+          onClick={ () => setDisplayInputSearch(!displayInputSearch) }
+          type="button"
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="Search icon"
+          />
+        </button>
+      )}
+      {displayInputSearch !== false && <input data-testid="search-input" type="text" />}
     </header>
   );
 }
