65e3b709b3d5414f6a67ac513bcfeeb2ad4bfb74
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-01-28 16:42:04 -0300

    feat: req/11 - Redireciona ao clicar no icon profile para '/profile'

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
@@ -6,7 +7,9 @@ import searchIcon from '../../images/searchIcon.svg';
 function Header({ children, displayIconSearch }) {
   return (
     <header>
-      <img data-testid="profile-top-btn" src={ profileIcon } alt="Profile icon" />
+      <Link to="/profile">
+        <img data-testid="profile-top-btn" src={ profileIcon } alt="Profile icon" />
+      </Link>
       <p data-testid="page-title">{children}</p>
       {displayIconSearch !== false && (
         <img data-testid="search-top-btn" src={ searchIcon } alt="Search icon" />)}
