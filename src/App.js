5843ef317f10e142931709980b50607ea8bf6538
FernandoMos92 <fernandomosp@gmail.com>
2022-01-28 11:48:17 -0300

    feat: Completed task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Login from './Components/Login';
+import globalContext from './Context/globalContext';
 
 function App() {
+  const [email, setEmail] = React.useState('');
+  const [btnLogin, setBtnLogin] = React.useState(true);
+  const [password, setPassword] = React.useState('');
+
+  const contextValue = {
+    email,
+    setEmail,
+    btnLogin,
+    setBtnLogin,
+    password,
+    setPassword,
+  };
+
   return (
-    <Login />
+    <globalContext.Provider value={ contextValue }>
+      <Login />
+    </globalContext.Provider>
   );
 }
 
