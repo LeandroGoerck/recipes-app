33ddce374912bf1d6dd3419a6d0e7c0959ab5c87
Mauricio Lopes <mlopeesz@gmail.com>
2022-01-30 10:54:38 -0300

    feat: pagina Profile completa

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function Profile() {
+  const emailStorage = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header displayIconSearch={ false }>Profile</Header>
+      <div>
+        <p data-testid="profile-email">{emailStorage.email}</p>
+
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ handleLogout }
+        >
+          Logout
+        </button>
+
+      </div>
       <Footer />
     </div>
   );
