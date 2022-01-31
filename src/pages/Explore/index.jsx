f6eb9cdb3984f425ebb0376a94a5f87cd8e57b55
Mauricio Lopes <mlopeesz@gmail.com>
2022-01-31 10:36:07 -0300

    feat: implementado pagina explore

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function Explore() {
+  const history = useHistory();
+
   return (
     <div>
       <Header displayIconSearch={ false }>Explore</Header>
+      <div>
+        <button
+          data-testid="explore-foods"
+          type="button"
+          onClick={ () => history.push('/explore/foods') }
+        >
+          Explore Foods
+        </button>
+        <button
+          data-testid="explore-drinks"
+          type="button"
+          onClick={ () => history.push('/explore/drinks') }
+        >
+          Explore Drinks
+        </button>
+      </div>
       <Footer />
     </div>
   );
