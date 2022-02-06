c634513cdaee4d0ad0ac58dd2ad70d09103f3beb
Mauricio Lopes <mlopeesz@gmail.com>
2022-02-06 13:26:13 -0300

    feat: requisito 77

--- src/pages/Foods/index.jsx
+++ src/pages/Foods/index.jsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import CardList from '../../components/CardList';
-import GlobalContext from '../../Context/GlobalContext';
 import Categories from '../../components/Categories';
 
 function Foods() {
-  const { requestAPI: { firstTwelveFoods } } = useContext(GlobalContext);
-
-  useEffect(() => firstTwelveFoods(), []);
-
   return (
     <>
       <Header>Foods</Header>
